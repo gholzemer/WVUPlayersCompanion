fb68ec579e1336a0fc54f1ef4bfe9af97d69bdf6
gholzemer <gh0026@mix.wvu.edu>
2023-02-07 10:59:01 -0500

    comments

--- index.js
+++ index.js
@@ -2,6 +2,11 @@
  * @format
  */
 
+// compilation errors:
+// pod deintegrate
+// pod clean
+// pod install
+
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
