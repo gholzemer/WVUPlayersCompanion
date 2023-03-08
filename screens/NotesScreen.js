4774f62dd0d2bcbeef6242ec02d4eb6a5d52ea63
drewgriffith123 <47809840+drewgriffith123@users.noreply.github.com>
2023-03-08 13:07:19 -0500

    Notes Page Stub

--- screens/NotesScreen.js
+++ screens/NotesScreen.js
@@ -1,10 +1,23 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, TextInput, Button} from 'react-native';
 
 function NotesScreen() {
+  const [value, onChangeText] = React.useState('');
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      
+
+      <TextInput
+        editable
+        autoFocus
+        multiline
+        numberOfLines={4}
+        placeholder='Enter Notes'
+        onChangeText={text => onChangeText(text)}
+        value={value}
+        style={{padding: 10}}
+      />
+   <Button title="Submit"
+        onPress={null} />
     </View>
   );
 }
